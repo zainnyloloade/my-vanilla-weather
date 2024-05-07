428b5cc64ffeebf8bf8ae9ea0e7e66f5bf549179
zainnyloloade <168852927+zainnyloloade@users.noreply.github.com>
2024-05-07 17:01:45 +0100

    added apiUrl link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function refreshWeather (response){
+let tempElement = document.querySelector("#temp");
+let temp = response.data.temp.current 
+
+tempElement.innerHTML = Math.round(temp);
+   
+}
+function searchCity(city) {
+ let apiKey = "fa49b3b035f29f4a1887od48f7teb11d";
+ let apiUrl = 
+ `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
