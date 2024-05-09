99b721898fa3b84f991ccf051cf0ef200637e4a9
zainnyloloade <168852927+zainnyloloade@users.noreply.github.com>
2024-05-09 16:41:35 +0100

    added the forecast html and css

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-
+    let iconElement = document.querySelector("#icon")
+    
     console.log(response.data);
 
+    icon.innerHTML = `<img src="${response.data.condition.icon_url}"
+    class="weather-app-icon" />`;
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
+    temperatureElement.innerHTML = Math.round(temperature);  
 }
 
 function formatDate(date) {
