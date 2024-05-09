9f984ffce74618fd8eb27ed92aa18cf4420a31af
zainnyloloade <168852927+zainnyloloade@users.noreply.github.com>
2024-05-09 12:32:46 +0100

    added Function to fetch and display current weather data for a specified city.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,45 @@
-function refreshWeather (response){
-let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.temperature.current 
-let cityElement = document.querySelector("#city");
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
-cityElement.innerHTML = response.data.city;
-temperatureElement.innerHTML = Math.round(temperature);
-   
+    console.log(response.data);
+
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+}
+
 function searchCity(city) {
- let apiKey = "fa49b3b035f29f4a1887od48f7teb11d";
- let apiUrl = 
- `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiKey = "fa49b3b035f29f4a1887od48f7teb11d";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(refreshWeather);
+    axios.get(apiUrl).then(refreshWeather);
 }
-
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    
-  
     searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Lagos")
+searchCity("Lagos");
+
